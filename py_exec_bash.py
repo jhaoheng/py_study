c097bf9519161331d33b657e549df050664300a5
MAX HU <jhaoheng@gmail.com>
2016-02-01 07:58:44 +0800

    test exec bash

---
+++ py_exec_bash.py
@@ -0,0 +1,16 @@
+import subprocess, sys
+
+def Test( ):
+    # subprocess.call(shlex.split('./test.sh param1 param2'))
+    """
+    retcode = subprocess.call(['./tbash.sh'])
+    print "test"
+    """
+
+    cmd = 'ls -l'
+    retcode = subprocess.call(cmd, shell=True)
+    if retcode != 0: sys.exit(retcode)
+
+if __name__ == "__main__" :
+
+    Test( )
